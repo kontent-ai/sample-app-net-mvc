94041ef73a5fd799db2555c702c0539cabaf97ad
Daniel Pokorny <52500882+pokornyd@users.noreply.github.com>
2026-01-13 15:49:48 +0100

    delete leftover service interface

--- Services/Content/Interfaces/IContentService.cs
+++
@@ -1,8 +0,0 @@
-using Kontent.Ai.Delivery.Abstractions;
-
-namespace Ficto.Services.Content.Interfaces;
-
-public partial interface IContentService
-{
-    Task<IDeliveryResult<IContentItem<T>>> GetItemAsync<T>(string codename) where T : class, IElementsModel;
-}
