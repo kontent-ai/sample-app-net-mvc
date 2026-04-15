df57e158548d103766955263adb55590cc56e061
Daniel Pokorny <52500882+pokornyd@users.noreply.github.com>
2026-04-15 14:06:26 +0200

    remove duplicit options from appSettings

--- Models/Helpers/RouteResolver.cs
+++ Models/Helpers/RouteResolver.cs
@@ -12,7 +12,7 @@ public class RouteResolver(IOptionsMonitor<SiteOptions> options) : IRouteResolve
 {
     public string ResolveUrl(Reference? reference) => reference switch
     {
-        ItemReference { Type: { } type, Slug: { } slug } =>
+        ItemReference { Type: not null and var type, Slug: not null and var slug } =>
             options.CurrentValue.RouteTemplates.TryGetValue(type, out var template)
                 ? template.Replace("{slug}", slug)
                 : $"/{type}/{slug}",
